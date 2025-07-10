98965afc8eed12931300629baf0dc495298e2929
Jordan0725 <161843035+Jordan0725@users.noreply.github.com>
2025-07-09 21:45:45 -0300

    Add files via upload

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,79 +1,24 @@
-import React from 'react'
+import React from "react";
+import Coment from "./Comentarios"
+
+const mensagens = [1, 2, 3, 4];
 
 function Lista() {
   return (
-    <div class="messages-container">
-    <h1>Mensagens:</h1>
-    <div class="messages-list">
-        <div class="message-card">
-            <div class="message-meta">
-                <div class="delete-icon">
-                    <i class="fas fa-trash"></i>
-                </div>
-                <div class="message-details">
-                    <div>ID:123456</div>
-                    <div>04/07/2025</div>
-                </div>
-            </div>
-            <div class="message-content">
-                <h3 class="user-name">Usuário de teste</h3>
-                <p class="message-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus tortor lacus sem phasellus ante suspendisse odio odio ultricies venenatis adipiscing. Nullam erat quam volutpat adipiscing vel vitae pulvinar neque vitae. Eget eget laoreet vivamus tempus donec rhoncus pulvinar varius ante ipsum malesuada."</p>
-            </div>
-            <div class="options-dot"></div>
+    <div className="Lista-container">
+        <div className="mensagens-header">
+            <h2 className="title">Mensagens:</h2>
         </div>
-
-        <div class="message-card">
-            <div class="message-meta">
-                <div class="delete-icon">
-                    <i class="fas fa-trash"></i>
-                </div>
-                <div class="message-details">
-                    <div>ID:123456</div>
-                    <div>04/07/2025</div>
-                </div>
-            </div>
-            <div class="message-content">
-                <h3 class="user-name">Usuário de teste</h3>
-                <p class="message-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus tortor lacus sem phasellus ante suspendisse odio odio ultricies venenatis adipiscing. Nullam erat quam volutpat adipiscing vel vitae pulvinar neque vitae. Eget eget laoreet vivamus tempus donec rhoncus pulvinar varius ante ipsum malesuada."</p>
-            </div>
-            <div class="options-dot"></div>
-        </div>
-
-        <div class="message-card">
-            <div class="message-meta">
-                <div class="delete-icon">
-                    <i class="fas fa-trash"></i>
-                </div>
-                <div class="message-details">
-                    <div>ID:123456</div>
-                    <div>04/07/2025</div>
-                </div>
-            </div>
-            <div class="message-content">
-                <h3 class="user-name">Usuário de teste</h3>
-                <p class="message-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus tortor lacus sem phasellus ante suspendisse odio odio ultricies venenatis adipiscing. Nullam erat quam volutpat adipiscing vel vitae pulvinar neque vitae. Eget eget laoreet vivamus tempus donec rhoncus pulvinar varius ante ipsum malesuada."</p>
-            </div>
-            <div class="options-dot"></div>
-        </div>
-         <div class="message-card">
-            <div class="message-meta">
-                <div class="delete-icon">
-                    <i class="fas fa-trash"></i>
-                </div>
-                <div class="message-details">
-                    <div>ID:123456</div>
-                    <div>04/07/2025</div>
-                </div>
-            </div>
-            <div class="message-content">
-                <h3 class="user-name">Usuário de teste</h3>
-                <p class="message-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Metus tortor lacus sem phasellus ante suspendisse odio odio ultricies venenatis adipiscing. Nullam erat quam volutpat adipiscing vel vitae pulvinar neque vitae. Eget eget laoreet vivamus tempus donec rhoncus pulvinar varius ante ipsum malesuada."</p>
-            </div>
-            <div class="options-dot"></div>
+    <div className="messages-container">
+        <div className="messages-list">
+            <Coment/>
+            <Coment/>
+            <Coment/>
+            <Coment/>
         </div>
     </div>
 </div>
-  )
+  );
 }
 
-export default Lista
+export default Lista;
